dd1c629a32192dd2bde063e01efffc9c1248ecff
Flavio Coscarella <flaviodc@live.com.ar>
2023-03-22 10:15:50 -0300

    Ejercicio 2

---
+++ ej2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] arrayString = new String[]{"Eduardo", "Jorge", "Luna"};
+        for (String s : arrayString) {
+            System.out.println(s);
+        }
+    }
+}
